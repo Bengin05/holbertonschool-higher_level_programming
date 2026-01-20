17ea7b808218850f25edc2e796e0cc5eca93b56e
Bengin05 <uzunbengin9@gmail.com>
2026-01-20 13:30:06 +0000

    i have creation the file for the add script computes the square value of all integers of a matrix

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+
+    new_matrix = []
+
+    for row in matrix:
+        new_row = []
+        for number in row:
+            square = number * number
+            new_row.append(square)
+        new_matrix.(new_row)
+
+    return new_matrix
