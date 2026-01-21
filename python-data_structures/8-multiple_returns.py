9c8c26dd9086d1f2fe43015c4c1586505b0aeaed
Bengin05 <uzunbengin9@gmail.com>
2026-01-21 10:41:43 +0000

    i have the modification the script

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-
     first = ""
 
     if sentence == "":
-        sentence = None
+        first = None
     else:
         first = sentence[0]
 
-    length = len(sentence)
-    return (sentence, first)
+    lenght = len(sentence)
+    return (lenght, first)
