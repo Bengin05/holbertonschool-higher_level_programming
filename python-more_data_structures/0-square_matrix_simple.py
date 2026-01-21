d220b30205c8e21eb9e39cee9122d54224e53eef
Bengin05 <uzunbengin9@gmail.com>
2026-01-21 10:51:13 +0000

    i have modification the script

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-
     new_matrix = []
 
     for row in matrix:
@@ -8,6 +7,6 @@ def square_matrix_simple(matrix=[]):
         for number in row:
             square = number * number
             new_row.append(square)
-        new_matrix.(new_row)
+        new_matrix.append(new_row)
 
     return new_matrix
