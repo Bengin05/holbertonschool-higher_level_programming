f20faa640422f6307322760542debd76bbc10255
Bengin05 <uzunbengin9@gmail.com>
2026-01-21 11:10:55 +0000

    i add the script for returns a key with the biggest integer value.

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    best_value = 0
+    best_key = None
+
+    if not a_dictionary:
+        return None
+
+    for key in a_dictionary:
+        value = a_dictionary[key]
+
+        if best_value is None or value > best_value:
+            best_value = value
+            best_key = key
+    return best_key
