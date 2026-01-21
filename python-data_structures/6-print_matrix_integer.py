76616ddea7f1187977cc121cd2eda9c091b62bdb
Bengin05 <uzunbengin9@gmail.com>
2026-01-21 10:48:31 +0000

    i have the modification the script

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-
     for row in matrix:
         for i in range(len(row)):
-            if i =! 0:
+            if i != 0:
                 print(" ", end="")
-            print("{:d}". format(row[i]), end="")
+            print("{:d}".format(row[i]), end="")
         print()
