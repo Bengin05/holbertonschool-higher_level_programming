ef81521d83831eb6788e3025abdbbad3c19a7444
Bengin05 <uzunbengin9@gmail.com>
2026-01-28 10:04:43 +0000

    i have add the pycodestyle

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """This module defines the Square class for basic OOP practice."""
+
+
 class Square:
     """Represents a square with a private size attribute."""
+
     def __init__(self, size):
         """Initialize a new Square instance.
 
