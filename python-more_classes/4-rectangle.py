10321287d9080ca2bd788e292b0033eefa5c1a2e
Bengin05 <uzunbengin9@gmail.com>
2026-01-29 11:27:54 +0000

    i have add the script for the task 4

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""Module that defines the Rectangle class with width and height properties."""
+
+
+class Rectangle:
+    """Represents a rectangle with width and height."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle instance."""
+        self.width = width
+        self.height = height
+
+    def __repr__(self):
+        """Return a string representation to recreate the rectangle."""
+        return "Rectangle({}, {})".format(self.width, self.height)
+
+    @property
+    def width(self):
+        """Retrieve the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle with validation."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle with validation."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return the perimeter of the rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Return the rectangle as a string of '#' characters."""
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+        line = "#" * self.__width
+        lines = [line] * self.__height
+        return "\n".join(lines)
