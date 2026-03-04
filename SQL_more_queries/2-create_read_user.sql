dcf0b12e98e1db9dee2b53a9361a35e38c7d2930
Bengin.05 <uzunbengin9@gmail.com>
2026-03-04 12:12:49 +0000

    i pudate for the checking

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
--- Creates the new database hbtn_0d_2 and the user user_0d_2
+-- Create a new user in MySQL and grant SELECT privileges to that user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
+IDENTIFIED BY 'user_0d_2_pwd';
 
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
