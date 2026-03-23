79aef426e00a3a3154a4b9d443b6ff216368cfbd
Bengin.05 <uzunbengin9@gmail.com>
2026-03-23 15:17:12 +0000

    feat: add a script  that fetches and lists the title for all movies by using this URL: https://swapi-api.hbtn.io/api/films/?format=json
    All movie titles must be list in the HTML ul element with id list_movies
    You must use the Fetch API.

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,18 @@
+const listMovies = document.getElementById('list_movies');
+
+function fetchMovies() {
+  fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+    .then(response => response.json())
+    .then(data => {
+      data.results.forEach(movie => {
+        const li = document.createElement('li');
+        li.textContent = movie.title;
+        listMovies.appendChild(li);
+      });
+    })
+    .catch(error => {
+      console.error('Error fetching movies:', error);
+    });
+}
+
+fetchMovies();
